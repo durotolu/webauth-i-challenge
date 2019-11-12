a02736f05372870deae7b5c11ef8e544f19bfe8a
Modurotolu Olokode <durolawk@gmail.com>
2019-11-12 16:56:30 +0100

    created register react app

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+const initialForm = {
+  username: '',
+  password: '',
+};
+
+function App() {
+
+  const [signUpForm, setSignUpForm] = useState(initialForm);
+
+  const isDisabled = () => {
+    return !signUpForm.username || !signUpForm.password;
+  }
+
+  const onFormInput = e => {
+    setSignUpForm({...signUpForm, [e.target.id]: e.target.value});
+  }
+
+  const addUser = e => {
+    e.preventDefault();
+    const newUser = {
+      username: signUpForm.username,
+      password: signUpForm.password,
+    }
+    axios.post('http://localhost:4000/api/auth/register', newUser)
+      .then(res => {
+        console.log(res)
+        debugger
+      })
+      .catch(err => {
+        console.log(err)
+        debugger
+      })
+    setSignUpForm(initialForm);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form onSubmit={addUser}>
+          <label htmlFor="username">Username</label>
+          <input onChange={onFormInput} maxLength={50} value={signUpForm.username} id='username' type='text' />
+
+          <label htmlFor="password">Password</label>
+          <input onChange={onFormInput} value={signUpForm.password} id='password' type='password' />
+
+          <button disabled={isDisabled()}>Submit</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
